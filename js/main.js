9a3e39824d104f6b593f48ab1572575121c9c421
samith <samith@misynergy.com>
2022-04-07 11:02:57 +0530

    Typrewriter animation fixed.

--- js/main.js
+++ js/main.js
@@ -7,20 +7,39 @@ collapsibles.forEach((item) =>
 );
 
 // Typewriter Animation
-var string =
-  "Hello everyone , 👋  I’m Samith Sankalpa"; /* type your text here */
-var array = string.split("");
-var timer;
+var aText = new Array("Hello everyone 👋", "I’m Samith Sankalpa");
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
 
-function frameLooper() {
-  if (array.length > 0) {
-    document.getElementById("text").innerHTML += array.shift();
-  } else {
-    clearTimeout(timer);
+var iTextPos = 0; // initialise text position
+var sContents = ""; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+  sContents = " ";
+  iRow = Math.max(0, iIndex - iScrollAt);
+  var destination = document.getElementById("typedtext");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + "<br />";
   }
-  loopTimer = setTimeout("frameLooper()", 70); /* change 70 for speed */
+  destination.innerHTML =
+    sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
 }
-frameLooper();
+
+typewriter();
 
 // Bottom to Top btn
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
