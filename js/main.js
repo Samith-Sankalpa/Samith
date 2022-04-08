5a5631979ac3456a86c8852c32c8116dba1ed816
samith <samith@misynergy.com>
2022-04-08 23:48:55 +0530

    About page cheked.

--- js/main.js
+++ js/main.js
@@ -20,12 +20,3 @@ function scrollToTop() {
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
 // Active Status
-var header = document.getElementById("activeNav");
-var btns = header.getElementsByClassName("active-tab");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
